bd239edef8355b7951d6d116e199be6ce99b24d7
aaronrc23 <aaronrc2022@gmail.com>
2024-02-28 17:34:33 -0500

    version v3.4.1

--- src/main/java/com/example/config/CorsConfig.java
+++ src/main/java/com/example/config/CorsConfig.java
@@ -14,13 +14,14 @@ public class CorsConfig {
                 @Override
                 public void addCorsMappings(CorsRegistry registry){
                     registry.addMapping("/login")
-                            .allowedOrigins("http://localhost:4200", "http://localhost:5173")
-//                                    .allowedOrigins("https://sysdatademofront.web.app/")
+//                            .allowedOrigins("http://localhost:4200", "http://localhost:5173")
+                                    .allowedOrigins("https://sysdatademofront.web.app/","https://upload-819f6.web.app/")
+
                                     .allowedMethods("*")
                                     .exposedHeaders("*");
                     registry.addMapping("/api/**")
-                            .allowedOrigins("http://localhost:4200", "http://localhost:5173")
-//                            .allowedOrigins("https://sysdatademofront.web.app/")
+//                            .allowedOrigins("http://localhost:4200", "http://localhost:5173")
+                            .allowedOrigins("https://sysdatademofront.web.app/","https://upload-819f6.web.app/")
                             .allowedMethods("*");
                 }
             };
